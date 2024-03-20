8b38c7afc7cd84d1c1d1b4b3cdb0b53a549cfae4
Sohan Jain <sohan@threads.com>
2024-03-20 16:55:42 -0500

    Add radix-ui

--- server/interview.ts
+++ server/interview.ts
@@ -1,9 +1,11 @@
 import ExampleClass from './ExampleClass';
 
-async function runScript() {
+async function main() {
   const counter = new ExampleClass();
   counter.incr(5);
   console.log(counter.get());
 }
 
-runScript();
+main();
+
+export default main;
--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,25 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { Button, Theme, Text, Flex } from '@radix-ui/themes';
+import '@radix-ui/themes/styles.css';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
   const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Theme accentColor="lime">
+        <Flex
+          direction="column"
+          gap="2"
+          width="max-content"
+          style={{ margin: 'auto' }}
+        >
+          <Text>Hello world</Text>
+          <Button onClick={() => setCount((c: number) => c + 1)}>
+            Count {count}
+          </Button>
+        </Flex>
+      </Theme>
     </>
   );
 }
